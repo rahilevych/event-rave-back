bd102b5ca464b0d1d0b49a52b42e0e471c61a817
Alona Rahilevych <alona.rahilevych@gmail.com>
2025-10-10 20:32:14 +0200

    reafactor: add validation decorators

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,9 @@
+import { IsEmail, IsString } from "class-validator";
+
 export class LoginDto {
+    @IsEmail()
     email!: string;
-    password!: string
+
+    @IsString()
+    password!: string;
 }
