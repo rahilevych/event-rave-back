85a75f84d11026cde795ed574fabd73daa4e9978
Alona Rahilevych <alona.rahilevych@gmail.com>
2025-10-12 11:08:27 +0200

    docs:add Swagger documentation for user and auth endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,22 @@
-import { BadRequestException, Body, Controller, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Post, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import express from 'express';
+import { ApiBadRequestResponse, ApiBody, ApiCookieAuth, ApiInternalServerErrorResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
+
   constructor(private readonly authService: AuthService) { }
 
   @Post('login')
+  @ApiOperation({ summary: 'User log in' })
+  @ApiOkResponse({ description: 'User successfully logged in' })
+  @ApiNotFoundResponse({ description: 'User not found' })
+  @ApiBody({ type: LoginDto, description: 'Login credentials' })
+  @ApiCookieAuth('refreshToken')
+
   async login(@Body() dto: LoginDto, @Res({ passthrough: true }) res: express.Response) {
     const tokens = await this.authService.login(dto.email, dto.password);
 
@@ -18,26 +27,37 @@ export class AuthController {
 
     return tokens.accessToken;
   }
+
   @Post('logout')
+  @ApiOperation({ summary: 'User log out' })
+  @ApiOkResponse({ description: 'User successfully logged out' })
+  @ApiBadRequestResponse({ description: 'Refresh token is required' })
+  @ApiNotFoundResponse({ description: 'Refresh token not found' })
+  @ApiInternalServerErrorResponse({ description: 'Could not delete refresh token' })
+  @ApiCookieAuth('refreshToken')
+
   async logout(@Res({ passthrough: true }) res: express.Response, @Req() req: express.Request) {
     const refreshToken = req.cookies['refreshToken'];
-    if (!refreshToken) throw new BadRequestException('Refresh token missing');
     await this.authService.logout(refreshToken)
     res.cookie('refreshToken', '', {
       httpOnly: true,
       maxAge: 0,
     });
     return { message: "User successfully logged out" }
-
   }
 
   @Post('refresh')
-  async refresh(@Req() req: express.Request, @Res({ passthrough: true }) res: express.Response) {
-    const refreshToken = req.cookies['refreshToken'];
-    if (!refreshToken) throw new BadRequestException('Refresh token missing');
+  @ApiOperation({ summary: 'Refresh access and refresh tokens' })
+  @ApiOkResponse({ description: 'Tokens were updated' })
+  @ApiBadRequestResponse({ description: 'Refresh token is required' })
+  @ApiNotFoundResponse({ description: 'Refresh token not found' })
+  @ApiInternalServerErrorResponse({ description: 'Could not refresh tokens' })
+  @ApiCookieAuth('refreshToken')
 
-    const tokens =
-      await this.authService.refreshTokens(refreshToken);
+  async refresh(@Req() req: express.Request, @Res({ passthrough: true }) res: express.Response) {
+
+    const refreshToken = req.cookies['refreshToken'];
+    const tokens = await this.authService.refreshTokens(refreshToken);
     res.cookie('refreshToken', tokens.refreshToken, {
       httpOnly: true,
       maxAge: 15 * 24 * 60 * 60 * 1000,
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,16 +11,27 @@ import {
 
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-
+import { ApiBody, ApiConflictResponse, ApiInternalServerErrorResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) { }
 
   @Post('register')
+  @ApiOperation({ summary: 'Create user' })
+  @ApiOkResponse({ description: 'User successfully registered' })
+  @ApiConflictResponse({ description: 'Email is already in use' })
+  @ApiInternalServerErrorResponse({ description: 'Could not create user' })
+  @ApiBody({ type: CreateUserDto, description: 'Create user data' })
+
   async register(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
   }
 
+  @ApiOperation({ summary: 'Delete user' })
+  @ApiOkResponse({ description: 'User successfully deleted' })
+  @ApiNotFoundResponse({ description: 'User not found' })
+  @ApiInternalServerErrorResponse({ description: 'Could not delete user' })
 
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number) {
