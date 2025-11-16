5db6753867172c560e9842f4285f7f79951c2a94
Alona Rahilevych <ragilevic@gmail.com>
2025-11-16 19:56:19 +0100

    feat: add swager documentation

--- src/likes/likes.controller.ts
+++ src/likes/likes.controller.ts
@@ -8,12 +8,31 @@ import {
 } from '@nestjs/common';
 import { LikesService } from './likes.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('likes')
 @Controller('likes')
 export class LikesController {
   constructor(private readonly likesService: LikesService) {}
+
   @UseGuards(JwtAuthGuard)
   @Put(':eventId/toggle')
+  @ApiOperation({ summary: 'Toggle like' })
+  @ApiOkResponse({ description: 'Like was toggled' })
+  @ApiInternalServerErrorResponse({
+    description: 'Server error toggling like',
+  })
+  @ApiBadRequestResponse({
+    description: 'Incorrect params',
+  })
+  @ApiBody({ type: Number, description: 'Event id' })
   async toggleLike(
     @Param('eventId', ParseIntPipe) eventId: number,
     @Req() req: any,
