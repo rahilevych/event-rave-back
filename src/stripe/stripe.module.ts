eacd1addc05d184878a92dfe043bbaf7a4da0dfb
Alona Rahilevych <ragilevic@gmail.com>
2026-01-14 12:18:15 +0100

    refactor:update  stripe module

--- src/stripe/stripe.module.ts
+++ src/stripe/stripe.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { StripeService } from './stripe.service';
 import { StripeController } from './stripe.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Module({
-  providers: [StripeService],
-  controllers: [StripeController]
-})
-export class StripeModule {}
+@Module({})
+export class StripeModule {
+  static forRootAsync(): DynamicModule {
+    return {
+      module: StripeModule,
+      imports: [ConfigModule],
+      providers: [
+        StripeService,
+        {
+          provide: 'STRIPE_API_KEY',
+          useFactory: (configService: ConfigService) =>
+            configService.get<string>('STRIPE_API_KEY'),
+          inject: [ConfigService],
+        },
+      ],
+      controllers: [StripeController],
+      exports: [StripeService],
+    };
+  }
+}
